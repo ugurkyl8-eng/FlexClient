4cad2980aac6aaaa3317b533b1ba33588ee9fae1
UĞUR CAN KÖYLÜ <252947653+ugurkyl8-eng@users.noreply.github.com>
2026-06-26 09:46:07 +0300

    Implement GUI key binding and update log message
    Added key binding for GUI and updated initialization message.

--- src/main/java/com/flex/client/FlexClient.java
+++ src/main/java/com/flex/client/FlexClient.java
@@ -1,16 +1,38 @@
 package com.flex.client;
 
+import com.flex.client.gui.ClickGui;
 import com.flex.client.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class FlexClient implements ClientModInitializer {
     public static final String MOD_ID = "flexclient";
     public static FlexClient INSTANCE;
+    private static KeyBinding guiKey;
 
     @Override
     public void onInitializeClient() {
         INSTANCE = this;
         ModuleManager.init();
-        System.out.println("[FlexClient] Yüklendi!");
+
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "FlexClient GUI",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "FlexClient"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (guiKey.wasPressed()) {
+                client.setScreen(new ClickGui());
+            }
+        });
+
+        System.out.println("[FlexClient] Yüklendi! RSHIFT ile GUI aç.");
     }
 }
